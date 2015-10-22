20766dad3ca2845f9c3470b0e77b1e5c0f3d326e
anshusharma1983 <anshusharma1983@gmail.com>
2015-10-22 19:25:51 +0530

    Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,146 +1,216 @@
-
-Backbone.Model.prototype.idAttribute = '_id';
+var pleaseWaitDiv = $('<div class="modal fade" style=top:40% id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Processing...</h4></div><div class="modal-body"><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div></div></div></div></div>');
 var fileURL="";
 var loggedInUsername="";
+var currentblog = -1;
+var file;
+var blogs = {}; 
 $.parse.init({
     app_id : "Hb2JQX1qTc5Jf6wNqLgTXSDFvP9xgrCHVgAkfKEv", // <-- enter your Application Id here 
     rest_key : "7M16XEm4RSoagE02H1R1apFZFQkk3JNG6NLvH3XG" // <--enter your REST API Key here    
 });
-// Backbone Model
 
-var Blog = Backbone.Model.extend({
-	defaults: {
-		author: '',
-		title: '',
-		url: ''
+$(document).ready(function() {
+	tryCookieLogin();
+	initializeFileUpload();
+	initializeAddBlog();
+	initializeTabs();
+	initializeRegisterPage();	
+});
+
+function initializeTabs(){
+	$('#tab-publish').click(function() {
+		$(".class-content").hide();
+		$(".publish-content").show();
+		$(".class-tab").removeClass("active");
+		$('#tab-publish').addClass("active");
+				
+	});
+
+	$("#tab-view").click(function(){
+		viewArticle(null);
+	});
+
+	$("#tab-list").click(function(){
+		$(".class-content").hide();
+		$(".list-content").show();
+		$(".class-tab").removeClass("active");
+		$('#tab-list').addClass("active");	
+		render();	
+	});
+}
+
+function viewArticle(obj){
+	$(".class-content").hide();
+	$(".view-content").show();
+	$(".class-tab").removeClass("active");
+	$('#tab-view').addClass("active");
+
+	if (Object.keys(blogs).length === 0) {
+		render({viewarticle : true});
+		return;
 	}
-});
-
-// Backbone Collection
-
-var Blogs = Backbone.Collection.extend({
-	url: 'https://api.parse.com/1/classes/Blog'
-});
-
-// instantiate two Blogs
-
-/*var blog1 = new Blog({
-	author: 'Michael',
-	title: 'Michael\'s Blog',
-	url: 'http://michaelsblog.com'
-});
-var blog2 = new Blog({
-	author: 'John',
-	title: 'John\'s Blog',
-	url: 'http://johnsblog.com'
-});*/
-
-// instantiate a Collection
-
-var blogs = new Blogs();
-
-// Backbone View for one blog
-
-var BlogView = Backbone.View.extend({
-	model: new Blog(),
-	tagName: 'tr',
-	initialize: function() {
-		this.template = _.template($('.blogs-list-template').html());
-	},
-	events: {
-		'click .edit-blog': 'edit',
-		'click .update-blog': 'update',
-		'click .cancel': 'cancel',
-		'click .delete-blog': 'delete'
-	},
-	edit: function() {
-		$('.edit-blog').hide();
-		$('.delete-blog').hide();
-		this.$('.update-blog').show();
-		this.$('.cancel').show();
-
-		var author = this.$('.author').html();
-		var title = this.$('.title').html();
-		var url = this.$('.url').html();
-
-		this.$('.author').html('<input type="text" class="form-control author-update" value="' + author + '">');
-		this.$('.title').html('<input type="text" class="form-control title-update" value="' + title + '">');
-		this.$('.url').html('<input type="text" class="form-control url-update" value="' + url + '">');
-	},
-		update: function() {
-		this.model.set('author', $('.author-update').val());
-		this.model.set('title', $('.title-update').val());
-		this.model.set('url', $('.url-update').val());
-
-		this.model.save(null, {
-			success: function(response) {
-				console.log('Successfully UPDATED blog with _id: ' + response.toJSON()._id);
-			},
-			error: function(err) {
-				console.log('Failed to update blog!');
-			}
-		});
-	},
-	cancel: function() {
-		blogsView.render();
-	},
-	delete: function() {
-		this.model.destroy({
-			success: function(response) {
-				console.log('Successfully DELETED blog with _id: ' + response.toJSON()._id);
-			},
-			error: function(err) {
-				console.log('Failed to delete blog!');
-			}
-		});
-	},
-	render: function() {
-		this.$el.html(this.template(this.model.toJSON()));
-		return this;
+	showArticle(obj);
+}
+function showArticle(obj){
+	if (obj != null) {
+		console.log("view blog :" + $(obj).closest('tr').index());	
+		currentblog = $(obj).closest('tr').index();
+	}else if (currentblog === -1){
+			currentblog = Object.keys(blogs).length -1;
 	}
-});
+	if (currentblog === -1) {
+		alert("Unable to load blog");
+		return;
+	}
+	console.log("showing blog" + currentblog);
+	var blog = blogs[currentblog];
+	$(".viewblog-image").html('<img src="' +blog.fileURL + '" style="width:100%"/>');
+	$(".viewblog-title").html(blog.title);
+	$(".viewblog-url").html(_.unescape(blog.url));
+}
 
-// Backbone View for all blogs
+function editArticle(obj){
+	console.log("edit blog :" + $(obj).closest('tr').index());	
+}
 
-var BlogsView = Backbone.View.extend({
-	model: blogs,
-	el: $('.blogs-list'),
-	initialize: function() {
-		var self = this;
-		this.model.on('add', this.render, this);
-		this.model.on('change', function() {
-			setTimeout(function() {
-				self.render();
-			}, 30);
-		},this);
-		this.model.on('remove', this.render, this);
-		$.parse.get("Blog", function(response){
+function render(options){
+	var viewarticle = false;
+	if (arguments.length == 1) {
+		viewarticle = options.viewarticle;
+	}
+	
+	console.log("viewarticle in render:" + viewarticle);
+	pleaseWaitDiv.modal('show');
+		$.parse.get("Blog", {
+ 			order : "-createdAt"
+		}, function(response){
+			pleaseWaitDiv.modal('hide');
 	         	for (var i = 0; i <response.results.length; i++) {
 	         		var json = response.results[i];
-	         		var b = new Blog({author : json.author, title: json.title, url: _.unescape(json.url)});
-	         		blogs.add(b);
+	         		blogs[Object.keys(blogs).length] = json;	         		
 	         	}
+	         	var el = $('.blogs-list');
+				var template = _.template($('.blogs-list-template').html());
+				el.html('');
+				_.each(blogs, function(blog) {
+					blog.url = _.unescape(blog.url);		
+					el.append(template(blog));
+				});
+				$("#blogs-table").dataTable().fnDestroy();
+				$("#blogs-table").dataTable({
+					searching : false,
+					ordering : false
+
+				});	      	
+		}, function(){
+			pleaseWaitDiv.modal('hide');
 		});
-	},
-	render: function() {
-		var self = this;
-		this.$el.html('');
-		_.each(this.model.toArray(), function(blog) {
-			self.$el.append((new BlogView({model: blog})).render().$el);
+	
+}
+
+function initializeRegisterPage(){
+	$(".register-submit").click(function() {
+		// $("#register-form").validate();
+		var username = $("#register-email").val();
+		var password = $("#password").val();
+		if (username === "" || password === ""){
+			return false;
+		}
+		
+		$.parse.signup({ 
+			  username : username, 
+			  password : password, 
+			  email : username
+		},function(){
+			console.log("Successfully registered");
+			loggedInUsername = username;
+
+			$.parse.login(username, password, function(){
+				console.log("Successfully logged in");
+				postLogin(username, password);
+			}, function(){
+				console.log("Failed to login");
+				logout();
+			})
+		}, function(){
+			console.log("failed to register");
+			logout();
 		});
-		/*$("#blogs-table").DataTable({
-			searching:false,
-			ordering:false
-		});*/
-		return this;
+	});
+
+	$(".signin-submit").click(function(){
+		// $("#signin-form").validate();
+		var username = $("#signin-email").val();
+		var password = $("#signin-password").val();
+		if (username === "" || password === ""){
+			return false;
+		}
+		pleaseWaitDiv.modal('show');
+		$.parse.login(username, password, function(){
+			pleaseWaitDiv.modal('hide');
+			console.log("Successfully logged in");
+			loggedInUsername = username;
+			postLogin(username, password);
+		}, function(){
+			pleaseWaitDiv.modal('hide');
+			console.log("Failed to login");
+			logout();
+		});
+	});
+
+	$(".logout").click(function(){
+		logout();
+	});
+
+	jQuery('#register-form').validate({
+            rules : {
+                password : {
+                    minlength : 5
+                },
+                password_confirm : {
+                    minlength : 5,
+                    equalTo : "#password"
+                }
+            }
+        });
+}
+
+function tryCookieLogin(){
+	var logincookie = $.cookie('login-cookie');
+	if (logincookie) {
+		console.log('Found login cookie:' + logincookie);
+		var arr = logincookie.split(':');
+		pleaseWaitDiv.modal('show');
+		$.parse.login(arr[0], arr[1], function(){
+			pleaseWaitDiv.modal('hide');
+			console.log("Successfully logged in");
+			loggedInUsername = arr[0];
+			postLogin(arr[0], arr[1]);
+		}, function(){
+			pleaseWaitDiv.modal('hide');
+		});	
+	}else {
+		console.log('Login cookie not found');
 	}
-});
+}
 
-var blogsView = new BlogsView();
+function postLogin(username, password) {
+	$(".before-login").hide();
+	$(".after-login").show();
+	$.cookie('login-cookie', username + ':' + password, {expires: 365});
+	$("a.username").html(username);
+	$(".author-input").val(username);
+}
 
-$(function() {
-    var file;
-    // Set an event listener on the Choose File field.
+function logout() {
+	$(".before-login").show();
+	$(".after-login").hide();
+	$.removeCookie('login-cookie');
+}
+
+function initializeFileUpload(){
+	var file;
+	 // Set an event listener on the Choose File field.
     $('#fileselect').bind("change", function(e) {
       var files = e.target.files || e.dataTransfer.files;
       // Our file var now holds the selected file
@@ -172,15 +242,16 @@ $(function() {
         }
       });
     });
+    tinymce.init({
+	    selector: "textarea",	    
+	    toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link"
+	});
+   }
 
-
-  });
-
-$(document).ready(function() {
-	tryCookieLogin();
-	$('.add-blog').on('click', function() {
+   function initializeAddBlog(){
+    	$('.add-blog').on('click', function() {
 		$("#article-form").validate();
-		var blogJSON = new Object();
+		var blogJSON = {};
 		blogJSON.author = $('.author-input').val();
 		blogJSON.title = $('.title-input').val();
 		blogJSON.url = _.escape(tinyMCE.activeEditor.getContent());
@@ -192,120 +263,12 @@ $(document).ready(function() {
 			return false;
 		}
 		console.log(JSON.stringify(blogJSON));
-		var blog = new Blog(blogJSON);
-		blogs.add(blog);
 		$.parse.post('Blog',blogJSON, function(json){
-		  blogsView.render();
-		});
+		  alert("Data posted Successfully");
+		  blogs[Object.keys(blogs).length] = blogJSON;
+		  render();
+		});		
 	});
+   }
 
-	$('#tab-publish').click(function() {
-		$(".publish-content").show();
-		$(".list-content").hide();
-		$('#tab-publish').addClass("active");
-		$('#tab-edit').removeClass("active");		
-	});
-	$("#tab-edit").click(function(){
-		$(".publish-content").hide();
-		$(".list-content").show();
-		$('#tab-edit').addClass("active");
-		$('#tab-publish').removeClass("active");
-		blogsView.render();
-	});
-		tinymce.init({
-	    selector: "textarea",	    
-	    toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link"
-	});
-
-	$(".register-submit").click(function() {
-		// $("#register-form").validate();
-		var username = $("#register-email").val();
-		var password = $("#password").val();
-		if (username === "" || password === ""){
-			return false;
-		}
-		
-		$.parse.signup({ 
-			  username : username, 
-			  password : password, 
-			  email : username
-		},function(){
-			console.log("Successfully registered");
-			loggedInUsername = username;
-			$.parse.login(username, password, function(){
-				console.log("Successfully logged in");
-				postLogin(username, password);
-			}, function(){
-				console.log("Failed to login");
-				logout();
-			})
-		}, function(){
-			console.log("failed to register");
-			logout();
-		});
-	});
-
-	$(".signin-submit").click(function(){
-		// $("#signin-form").validate();
-		var username = $("#signin-email").val();
-		var password = $("#signin-password").val();
-		if (username === "" || password === ""){
-			return false;
-		}
-		$.parse.login(username, password, function(){
-			console.log("Successfully logged in");
-			loggedInUsername = username;
-			postLogin(username, password);
-		}, function(){
-			console.log("Failed to login");
-			logout();
-		});
-	});
-
-	$(".logout").click(function(){
-		logout();
-	});
-
-	jQuery('#register-form').validate({
-            rules : {
-                password : {
-                    minlength : 5
-                },
-                password_confirm : {
-                    minlength : 5,
-                    equalTo : "#password"
-                }
-            }
-        });
-	
-});
-
-function tryCookieLogin(){
-	var logincookie = $.cookie('login-cookie');
-	if (logincookie) {
-		console.log('Found login cookie:' + logincookie);
-		var arr = logincookie.split(':');
-		$.parse.login(arr[0], arr[1], function(){
-			console.log("Successfully logged in");
-			loggedInUsername = arr[0];
-			postLogin(arr[0], arr[1]);
-		});	
-	}else {
-		console.log('Login cookie not found');
-	}
-}
-
-function postLogin(username, password) {
-	$(".before-login").hide();
-	$(".after-login").show();
-	$.cookie('login-cookie', username + ':' + password, {expires: 365});
-	$("a.username").html(username);
-	$(".author-input").val(username);
-}
-
-function logout() {
-	$(".before-login").show();
-	$(".after-login").hide();
-	$.removeCookie('login-cookie');
-}
 
